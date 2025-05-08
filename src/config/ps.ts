0e8711509ac1b6b1cbd0034e2222a4cc61978174
Arsh Rastogi <79075464+Arsh-Rastogi@users.noreply.github.com>
2025-05-09 00:42:40 +0530

    Update ps.ts

--- src/config/ps.ts
+++ src/config/ps.ts
@@ -8,4 +8,4 @@ export const admins = _admins.map(toId);
 export const username = process.env.PS_USERNAME ?? 'Arsras';
 export const password = process.env.PS_PASSWORD ?? 'arsrasbotarsh';
 export const rooms = process.env.PS_ROOMS?.split(',') ?? ['botdevelopment'];
-export const prefix = process.env.PREFIX ?? ',';
+export const prefix = process.env.PREFIX ?? '%';
