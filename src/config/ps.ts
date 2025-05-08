984f739b3a742558e466bc3c4a7833b6a0a03570
Arsh Rastogi <79075464+Arsh-Rastogi@users.noreply.github.com>
2025-05-09 00:41:55 +0530

    Update ps.ts

--- src/config/ps.ts
+++ src/config/ps.ts
@@ -2,10 +2,10 @@ import { toId } from '@/tools';
 
 export const ranks = ['locked', 'muted', 'regular', 'whitelist', 'voice', 'driver', 'mod', 'bot', 'owner', 'admin'] as const;
 
-export const owner = process.env.PS_OWNER ?? 'PartMan';
+export const owner = process.env.PS_OWNER ?? 'Arsh_._.Rastogi';
 const _admins = process.env.PS_ADMINS?.split(/ *, */) ?? [];
 export const admins = _admins.map(toId);
-export const username = process.env.PS_USERNAME ?? 'PartBot';
-export const password = process.env.PS_PASSWORD ?? 'password';
+export const username = process.env.PS_USERNAME ?? 'Arsras';
+export const password = process.env.PS_PASSWORD ?? 'arsrasbotarsh';
 export const rooms = process.env.PS_ROOMS?.split(',') ?? ['botdevelopment'];
 export const prefix = process.env.PREFIX ?? ',';
